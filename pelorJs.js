50e3ea8d79bb1e51c33f09ec868157035da5917a
Nathaniel Wiley <nwiley86@gmail.com>
2017-10-10 10:50:17 -0400

    Update pelorJs.js

--- pelorJs.js
+++ pelorJs.js
@@ -1,6 +1,12 @@
 ;var pelorJs = pelorJs || {
 	init:function(){
 		alert('pelorJs init ran');
+		$.ajax({
+		  url: "/sample.json",
+		  dataType: "json"
+		}).success(function(data) {
+		  console.log(data);
+		});
 	}
 };
 console.log('pelorJs loaded');
